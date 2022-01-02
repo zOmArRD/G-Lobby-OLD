4a069184892c8db3483a6bbc4049a6fe08a849d3
zOmArRD <omarsena25@gmail.com>
2022-01-02 21:06:58 +0100

    Automatically generated class: LangKey.php

---
+++ src/zomarrd/ghostly/player/language/LangKey.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Copyright © 2022 (zOmArRD) GhostlyMC Network - All Rights Reserved.
+ *
+ * This is a self-generated file, don't try to modify it by hand lol
+ *
+ *      $$$$$$\  $$\                             $$\     $$\           $$\      $$\  $$$$$$\  
+ *     $$  __$$\ $$ |                            $$ |    $$ |          $$$\    $$$ |$$  __$$\ 
+ *     $$ /  \__|$$$$$$$\   $$$$$$\   $$$$$$$\ $$$$$$\   $$ |$$\   $$\ $$$$\  $$$$ |$$ /  \__|
+ *     $$ |$$$$\ $$  __$$\ $$  __$$\ $$  _____|\_$$  _|  $$ |$$ |  $$ |$$\$\$ $$ |$$ |      
+ *     $$ |\_$$ |$$ |  $$ |$$ /  $$ |\$$$$$\    $$ |    $$ |$$ |  $$ |$$ \$$  $$ |$$ |      
+ *     $$ |  $$ |$$ |  $$ |$$ |  $$ | \____$$\   $$ |$$\ $$ |$$ |  $$ |$$ |\$  /$$ |$$ |  $$\ 
+ *     \$$$$$  |$$ |  $$ |\$$$$$  |$$$$$$$  |  \$$$  |$$ |\$$$$$$ |$$ | \_/ $$ |\$$$$$  |
+ *      \______/ \__|  \__| \______/ \_______/    \____/ \__| \____$$ |\__|     \__| \______/ 
+ *                                                           $$\   $$ |                       
+ *                                                           \$$$$$  |                       
+ *                                                            \______/                                                             
+ */
+declare(strict_types=1);
+
+namespace zomarrd\ghostly\player\language;
+
+/**
+ * This class is generated automatically, do NOT modify it by hand.
+ */
+final class LangKey
+{
+	public const GLOBAL_FORM_BUTTON_CLOSE = "global.form.button.close";
+	public const GLOBAL_FORM_BUTTON_PREDETERMINED = "global.form.button.predetermined";
+	public const GLOBAL_PERMISSION_MESSAGE = "global.permission.message";
+	public const LANG_FORM_CONTENT = "lang.form.content";
+	public const LANG_FORM_TITLE = "lang.form.title";
+	public const LANG_MESSAGE_SET = "lang.message.set";
+	public const LANG_MESSAGE_SET_FAIL = "lang.message.set-fail";
+}
