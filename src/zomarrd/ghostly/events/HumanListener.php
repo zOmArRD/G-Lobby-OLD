88770e6bb614b2dddb7d2884031fbb3a57bf0154
zOmArRD <omarsena25@gmail.com>
2022-01-16 05:27:50 +0100

    Improved entity event handling (Some implementations)

--- src/zomarrd/ghostly/events/HumanListener.php
+++ src/zomarrd/ghostly/events/HumanListener.php
@@ -12,9 +12,11 @@ declare(strict_types=1);
 namespace zomarrd\ghostly\events;
 
 use pocketmine\event\Listener;
+use pocketmine\world\sound\ExplodeSound;
 use zomarrd\ghostly\entity\Entity;
 use zomarrd\ghostly\entity\events\HumanInteractEvent;
 use zomarrd\ghostly\player\language\LangKey;
+use zomarrd\ghostly\server\ServerManager;
 
 final class HumanListener implements Listener
 {
@@ -23,16 +25,27 @@ final class HumanListener implements Listener
 		$player = $event->getPlayer();
 		$entity = $event->getEntity();
 
-		switch ($entity->getNpcId()) {
-			case Entity::DISCORD:
-				$player->sendTranslated(LangKey::DISCORD_INVITATION_MESSAGE);
-				break;
-			case Entity::STORE:
-				$player->sendTranslated(LangKey::STORE_LINK_MESSAGE);
-				break;
-			case Entity::COMBO:
-				$player->sendMessage("work");
-				break;
+		$i = $entity->getNpcId();
+
+		if ($i === Entity::DISCORD) {
+			$player->sendTranslated(LangKey::DISCORD_INVITATION_MESSAGE);
+		}
+
+		if ($i === Entity::STORE) {
+			$player->sendTranslated(LangKey::STORE_LINK_MESSAGE);
+		}
+
+		if ($i === Entity::COMBO || $i === Entity::PRACTICE || $i === Entity::UHC || $i === Entity::UHC_RUN || $i === Entity::KITMAP || $i === Entity::HCF) {
+			$server = ServerManager::getInstance()->getServerByName($entity->getServerName());
+
+			if (is_null($server)) {
+				$player->knockBack(($player->getLocation()->x - ($entity->getLocation()->x + 0.5)), ($player->getLocation()->z - ($entity->getLocation()->z + 0.5)), (20 / 0xa));
+				$player->broadcastSound(new ExplodeSound(), [$player]);
+				$player->sendTranslated(LangKey::SERVER_CONNECT_ERROR_3);
+				return;
+			}
+
+			$player->transferTo($server);
 		}
 	}
 }
