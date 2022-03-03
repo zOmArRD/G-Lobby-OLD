d47a13e30e37bfa565193bdd1e2e59c977981890
zOmArRD <omarsena25@gmail.com>
2022-03-03 04:47:46 -0400

    idk

--- src/muqsit/simplepackethandler/interceptor/IPacketInterceptor.php
+++ src/muqsit/simplepackethandler/interceptor/IPacketInterceptor.php
@@ -7,42 +7,41 @@ namespace muqsit\simplepackethandler\interceptor;
 use Closure;
 use pocketmine\network\mcpe\NetworkSession;
 
-interface IPacketInterceptor
-{
+interface IPacketInterceptor{
 
-    /**
-     * @param Closure $handler
-     * @return IPacketInterceptor
-     *
-     * @phpstan-template TServerboundPacket of ServerboundPacket
-     * @phpstan-param Closure(TServerboundPacket, NetworkSession) : bool $handler
-     */
-    public function interceptIncoming(Closure $handler): IPacketInterceptor;
+	/**
+	 * @param Closure $handler
+	 * @return IPacketInterceptor
+	 *
+	 * @phpstan-template TServerboundPacket of ServerboundPacket
+	 * @phpstan-param Closure(TServerboundPacket, NetworkSession) : bool $handler
+	 */
+	public function interceptIncoming(Closure $handler) : IPacketInterceptor;
 
-    /**
-     * @param Closure $handler
-     * @return IPacketInterceptor
-     *
-     * @phpstan-template TClientboundPacket of ClientboundPacket
-     * @phpstan-param Closure(TClientboundPacket, NetworkSession) : bool $handler
-     */
-    public function interceptOutgoing(Closure $handler): IPacketInterceptor;
+	/**
+	 * @param Closure $handler
+	 * @return IPacketInterceptor
+	 *
+	 * @phpstan-template TClientboundPacket of ClientboundPacket
+	 * @phpstan-param Closure(TClientboundPacket, NetworkSession) : bool $handler
+	 */
+	public function interceptOutgoing(Closure $handler) : IPacketInterceptor;
 
-    /**
-     * @param Closure $handler
-     * @return IPacketInterceptor
-     *
-     * @phpstan-template TServerboundPacket of ServerboundPacket
-     * @phpstan-param Closure(TServerboundPacket, NetworkSession) : bool $handler
-     */
-    public function unregisterIncomingInterceptor(Closure $handler): IPacketInterceptor;
+	/**
+	 * @param Closure $handler
+	 * @return IPacketInterceptor
+	 *
+	 * @phpstan-template TServerboundPacket of ServerboundPacket
+	 * @phpstan-param Closure(TServerboundPacket, NetworkSession) : bool $handler
+	 */
+	public function unregisterIncomingInterceptor(Closure $handler) : IPacketInterceptor;
 
-    /**
-     * @param Closure $handler
-     * @return IPacketInterceptor
-     *
-     * @phpstan-template TClientboundPacket of ClientboundPacket
-     * @phpstan-param Closure(TClientboundPacket, NetworkSession) : bool $handler
-     */
-    public function unregisterOutgoingInterceptor(Closure $handler): IPacketInterceptor;
+	/**
+	 * @param Closure $handler
+	 * @return IPacketInterceptor
+	 *
+	 * @phpstan-template TClientboundPacket of ClientboundPacket
+	 * @phpstan-param Closure(TClientboundPacket, NetworkSession) : bool $handler
+	 */
+	public function unregisterOutgoingInterceptor(Closure $handler) : IPacketInterceptor;
 }
--- src/muqsit/simplepackethandler/monitor/IPacketMonitor.php
+++ src/muqsit/simplepackethandler/monitor/IPacketMonitor.php
@@ -7,42 +7,41 @@ namespace muqsit\simplepackethandler\monitor;
 use Closure;
 use pocketmine\network\mcpe\NetworkSession;
 
-interface IPacketMonitor
-{
+interface IPacketMonitor{
 
-    /**
-     * @param Closure $handler
-     * @return IPacketMonitor
-     *
-     * @phpstan-template TServerboundPacket of ServerboundPacket
-     * @phpstan-param Closure(TServerboundPacket, NetworkSession) : void $handler
-     */
-    public function monitorIncoming(Closure $handler): IPacketMonitor;
+	/**
+	 * @param Closure $handler
+	 * @return IPacketMonitor
+	 *
+	 * @phpstan-template TServerboundPacket of ServerboundPacket
+	 * @phpstan-param Closure(TServerboundPacket, NetworkSession) : void $handler
+	 */
+	public function monitorIncoming(Closure $handler) : IPacketMonitor;
 
-    /**
-     * @param Closure $handler
-     * @return IPacketMonitor
-     *
-     * @phpstan-template TClientboundPacket of ClientboundPacket
-     * @phpstan-param Closure(TClientboundPacket, NetworkSession) : void $handler
-     */
-    public function monitorOutgoing(Closure $handler): IPacketMonitor;
+	/**
+	 * @param Closure $handler
+	 * @return IPacketMonitor
+	 *
+	 * @phpstan-template TClientboundPacket of ClientboundPacket
+	 * @phpstan-param Closure(TClientboundPacket, NetworkSession) : void $handler
+	 */
+	public function monitorOutgoing(Closure $handler) : IPacketMonitor;
 
-    /**
-     * @param Closure $handler
-     * @return IPacketMonitor
-     *
-     * @phpstan-template TServerboundPacket of ServerboundPacket
-     * @phpstan-param Closure(TServerboundPacket, NetworkSession) : void $handler
-     */
-    public function unregisterIncomingMonitor(Closure $handler): IPacketMonitor;
+	/**
+	 * @param Closure $handler
+	 * @return IPacketMonitor
+	 *
+	 * @phpstan-template TServerboundPacket of ServerboundPacket
+	 * @phpstan-param Closure(TServerboundPacket, NetworkSession) : void $handler
+	 */
+	public function unregisterIncomingMonitor(Closure $handler) : IPacketMonitor;
 
-    /**
-     * @param Closure $handler
-     * @return IPacketMonitor
-     *
-     * @phpstan-template TClientboundPacket of ClientboundPacket
-     * @phpstan-param Closure(TClientboundPacket, NetworkSession) : void $handler
-     */
-    public function unregisterOutgoingMonitor(Closure $handler): IPacketMonitor;
+	/**
+	 * @param Closure $handler
+	 * @return IPacketMonitor
+	 *
+	 * @phpstan-template TClientboundPacket of ClientboundPacket
+	 * @phpstan-param Closure(TClientboundPacket, NetworkSession) : void $handler
+	 */
+	public function unregisterOutgoingMonitor(Closure $handler) : IPacketMonitor;
 }
