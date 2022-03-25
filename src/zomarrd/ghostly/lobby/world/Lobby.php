b09679bd0d5734637199f3d58e68ec2be1d4652a
zOmArRD <omarsena25@gmail.com>
2022-03-25 14:06:56 -0400

    Small update for test Lobby.php

--- src/zomarrd/ghostly/lobby/world/Lobby.php
+++ src/zomarrd/ghostly/lobby/world/Lobby.php
@@ -19,67 +19,76 @@ final class Lobby
     public static ?Lobby $instance = null;
     public Position $spawn;
 
-    public function __construct(private World $world, private int|float $spawnX, private int|float $spawnY, private int|float $spawnZ, private float|int $spawnYaw, private float|int $spawnPitch, private int $minVoid)
-    {
-        self::$instance = $this;
-        $this->spawn = new Position($this->getSpawnX(), $this->getSpawnY(), $this->getSpawnZ(), $this->getWorld());
-    }
+	public function __construct
+	(
+		private World     $world,
+		private int|float $spawnX,
+		private int|float $spawnY,
+		private int|float $spawnZ,
+		private float|int $spawnYaw,
+		private float|int $spawnPitch,
+		private int       $minVoid
+	)
+	{
+		self::$instance = $this;
+		$this->spawn = new Position($this->getSpawnX(), $this->getSpawnY(), $this->getSpawnZ(), $this->getWorld());
+	}
 
-    public function getSpawnX(): float|int
-    {
-        return $this->spawnX;
-    }
+	public function getSpawnX(): float|int
+	{
+		return $this->spawnX;
+	}
 
-    public function getSpawnY(): float|int
-    {
-        return $this->spawnY;
-    }
+	public function getSpawnY(): float|int
+	{
+		return $this->spawnY;
+	}
 
-    public function getSpawnZ(): float|int
-    {
-        return $this->spawnZ;
-    }
+	public function getSpawnZ(): float|int
+	{
+		return $this->spawnZ;
+	}
 
-    public function getWorld(): World
-    {
-        return $this->world;
-    }
+	public function getWorld(): World
+	{
+		return $this->world;
+	}
 
-    /**
-     * @return Lobby|null
-     */
-    public static function getInstance(): ?Lobby
-    {
-        return self::$instance;
-    }
+	/**
+	 * @return Lobby|null
+	 */
+	public static function getInstance(): ?Lobby
+	{
+		return self::$instance;
+	}
 
-    public function getDisplayName(): string
-    {
-        return $this->getWorld()->getDisplayName();
-    }
+	public function getDisplayName(): string
+	{
+		return $this->getWorld()->getDisplayName();
+	}
 
-    public function getFolderName(): string
-    {
-        return $this->getWorld()->getFolderName();
-    }
+	public function getFolderName(): string
+	{
+		return $this->getWorld()->getFolderName();
+	}
 
-    public function getSpawnYaw(): float|int
-    {
-        return $this->spawnYaw;
-    }
+	public function getSpawnYaw(): float|int
+	{
+		return $this->spawnYaw;
+	}
 
-    public function getSpawnPitch(): float|int
-    {
-        return $this->spawnPitch;
-    }
+	public function getSpawnPitch(): float|int
+	{
+		return $this->spawnPitch;
+	}
 
-    public function getMinVoid(): int
-    {
-        return $this->minVoid;
-    }
+	public function getMinVoid(): int
+	{
+		return $this->minVoid;
+	}
 
-    public function getSpawnPosition(): Position
-    {
-        return $this->spawn;
-    }
+	public function getSpawnPosition(): Position
+	{
+		return $this->spawn;
+	}
 }
