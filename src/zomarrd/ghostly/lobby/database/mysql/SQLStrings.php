04388bdba308e0364b754d41bca919c07c00e05e
zOmArRD <omarsena25@gmail.com>
2022-04-23 12:26:26 -0300

    WORKING ON BETTER TABLE SYSTEM

--- src/zomarrd/ghostly/lobby/database/mysql/SQLStrings.php
+++ src/zomarrd/ghostly/lobby/database/mysql/SQLStrings.php
@@ -13,37 +13,45 @@ namespace zomarrd\ghostly\lobby\database\mysql;
 
 final class SQLStrings
 {
-
-    public const CREATE_TABLE_SERVERS = 'CREATE TABLE IF NOT EXISTS `servers` (
-        name VARCHAR(26) UNIQUE,
-        ip VARCHAR(50),
-        port INT(5) UNIQUE,
-        online BOOLEAN,
-        maxplayers INT,
-        onlineplayers INT,
-        whitelisted BOOLEAN,
-        category VARCHAR(30)
+    public const CREATE_SERVERS_TABLE = 'CREATE TABLE IF NOT EXISTS `ghostly_servers` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `name` VARCHAR(26) NOT NULL,
+        `ip` VARCHAR(50) NOT NULL DEFAULT \'play.ghostlymc.live\',
+        `port` INT(5) NOT NULL DEFAULT 19132,
+        `online` BOOLEAN NOT NULL DEFAULT TRUE,
+        `maxplayers` INT NOT NULL DEFAULT 0,
+        `onlineplayers` INT NOT NULL DEFAULT 0,
+        `whitelisted` BOOLEAN NOT NULL DEFAULT TRUE,
+        `category` VARCHAR(30) NOT NULL DEFAULT \'Lobby\',
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `name` (`name`)
     );';
 
-    public const CREATE_PLAYER_DATA = "CREATE TABLE IF NOT EXISTS `player_data` (
-        xuid VARCHAR(50) UNIQUE,
-        username VARCHAR(50) UNIQUE,
-        scoreboard BOOLEAN DEFAULT TRUE,
-        language VARCHAR(16),
-        ip VARCHAR(50) DEFAULT '',
-        city VARCHAR(50) DEFAULT '',
-        region VARCHAR(50) DEFAULT '',
-        country VARCHAR(50) DEFAULT '',
-        continent VARCHAR(50) DEFAULT ''
-);";
+    public const CREATE_PLAYER_DATA_TABLE = 'CREATE TABLE IF NOT EXISTS `ghostly_playerdata` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `xuid` VARCHAR(50) NOT NULL,
+        `username` VARCHAR(50) NOT NULL,
+        `scoreboard` BOOLEAN DEFAULT TRUE,
+        `language` VARCHAR(16) DEFAULT \'en_EN\',
+        `ip` VARCHAR(50) DEFAULT \'\',
+        `city` VARCHAR(50) DEFAULT \'\',
+        `region` VARCHAR(50) DEFAULT \'\',
+        `country` VARCHAR(50) DEFAULT \'\',
+        `continent` VARCHAR(50) DEFAULT \'\',
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `xuid` (`xuid`)
+    );';
 
     public const CREATE_DEFAULT_TABLES = [
-        self::CREATE_TABLE_SERVERS,
-        self::CREATE_PLAYER_DATA,
+        self::CREATE_SERVERS_TABLE,
+        self::CREATE_PLAYER_DATA_TABLE,
     ];
 
     public const CREATE_DEFAULT_DB = 'CREATE DATABASE IF NOT EXISTS' . MySQL['prefix'] . '`ghostly`;';
 
-    public const INSERT_INTO_SERVERS = "INSERT INTO servers (name, ip, port, online, maxplayers, onlineplayers, whitelisted, category) VALUES ('%s', '%s', %s, %s, %s, %s, %s, '%s');";
+    /**
+     * TODO: Refactored to better handle the SQL strings.
+     */
+    public const INSERT_INTO_SERVERS = "INSERT INTO ghostly_servers (name, ip, port, online, maxplayers, onlineplayers, whitelisted, category) VALUES ('%s', '%s', %s, %s, %s, %s, %s, '%s');";
 }
 
